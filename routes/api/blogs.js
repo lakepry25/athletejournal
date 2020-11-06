69d8880a727939f26aac6123636cee8fd6e59913
Lake Pry <lakeandrewpry@gmail.com>
2020-11-06 16:25:18 -0500

    troubleshooting get all blogs

--- routes/api/blogs.js
+++ routes/api/blogs.js
@@ -49,7 +49,7 @@ router.post('/',
 router.get('/', async (req, res) => {
     try {
         const blogs = await Blog.find().sort({ date: -1 });
-        res.json(blogs);
+        res.json({ blogArray: blogs });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
