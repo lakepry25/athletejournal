9c9b204a1f3b40553e33e3d4814656c9818b9797
Lake Pry <lakeandrewpry@gmail.com>
2020-12-03 17:08:11 -0500

    All route to get all users

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,4 +74,17 @@ async (req, res) => {
 
 });
 
+// @route GET api/users
+// @desc Get all users
+// @access Public
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find().sort({ date: -1 });
+        res.json({ users: users });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
